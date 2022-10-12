7fba8dc8c72ca1ff7524672df67c2aa3402bee4d
Shova24 <jannatul35-2568@diu.edu.bd>
2022-10-12 10:48:03 +0600

    trush added

--- Server/src/Controllers/TaskController.js
+++ Server/src/Controllers/TaskController.js
@@ -9,6 +9,35 @@ export const getTasks = async (req, res) => {
   }
 };
 
+export const updateTrash = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Tasks.update({ is_deleted: "True" }, { where: { id: id } });
+    console.log("====================================");
+    console.log(req.body);
+    console.log("====================================");
+    res.status(200).json([req.body]);
+  } catch (err) {
+    res.status(404).json("Trash not Found");
+  }
+};
+export const deleteParmanent = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const task = await Tasks.findOne({ where: { id: id } });
+    if (task) {
+      const { taskName, priority, deadlineDate, starts, ends, is_deleted } = req.body;
+      await Tasks.update({ taskName, priority, deadlineDate, starts, ends, is_deleted }, { where: { id: id } });
+      res.status(200).json(Tasks);
+    } else {
+      res.status(200).json("task does not exist");
+    }
+    return;
+  } catch (err) {
+    res.status(404).json("Tasks not Found");
+  }
+};
+
 export const updateTasks = async (req, res) => {
   const id = req.params.id;
   try {
--- Server/src/routers/TasksRouter.js
+++ Server/src/routers/TasksRouter.js
@@ -1,8 +1,9 @@
 import Router from "express";
-import { getTasks, postTasks, updateTasks } from "../Controllers/TaskController";
+import { getTasks, postTasks, updateTasks, updateTrash } from "../Controllers/TaskController";
 const router = Router();
 
 router.get("/get-tasks", getTasks);
 router.post("/post-tasks", postTasks);
 router.patch("/update-task/:id", updateTasks);
+router.patch("/delete-task/:id", updateTrash);
 export default router;
